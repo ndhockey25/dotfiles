02537605882e79afefb11d76e4ed508a2cc55a29
ndhockey25 <brian.kachmarck@gmail.com>
2014-04-27 01:01:40 -0700

    Remove treating clicking in righthand corner of the track pad as a right click

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git pull origin master
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-      --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+      --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . 
+      --exclude ".gitconfig" ~
 source ~/.bash_profile
